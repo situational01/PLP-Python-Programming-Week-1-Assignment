83e395df745a6ada1a194454197704e54acd8044
situational <118795107+situational01@users.noreply.github.com>
2024-11-04 10:20:48 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# Asking for user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you from? ")
+
+# Displaying a personalized message
+print(f"Hello, {name}! You're {age} years old and from {location}. Welcome to Learning Python!")
